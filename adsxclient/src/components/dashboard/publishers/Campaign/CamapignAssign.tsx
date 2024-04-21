284a296ac1c4387fff9668a957e15dea8f0d2095
Ernst Kandombo <51041541+Ernstkandombo@users.noreply.github.com>
2024-04-21 23:34:15 +0200

    v

--- adsxclient/src/components/dashboard/publishers/Campaign/CamapignAssign.tsx
+++ adsxclient/src/components/dashboard/publishers/Campaign/CamapignAssign.tsx
@@ -1,22 +1,75 @@
-import React from 'react'
-import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogFooter,
-    DialogTitle,
-    DialogHeader,
-    DialogTrigger,
-} from "@/components/ui/dialog";
-import { Input } from "@/components/ui/input";
+
+'use client'
+
+
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogTitle, DialogHeader, DialogTrigger } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
-
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
+import { toast } from 'sonner';
 
 export default function AssignCampaign({ CampaignID }) {
-  return (
-    <Dialog>
-        
+    const [placementOptions, setPlacementOptions] = useState([]);
+    const [websiteOptions, setWebsiteOptions] = useState([]);
+    const [selectedPlacement, setSelectedPlacement] = useState("");
+    const [selectedWebsite, setSelectedWebsite] = useState("");
+    const [campaignIdResponse, setCampaignIdResponse] = useState(null); // State to store campaignId response
+
+    useEffect(() => {
+        // Fetch placement options
+        axios.get('http://localhost:5001/api/placement')
+            .then(response => {
+                setPlacementOptions(response.data);
+            })
+            .catch(error => {
+                console.error('Error fetching placement options:', error);
+            });
+
+        // Fetch website options
+        axios.get('http://localhost:5001/api/websites')
+            .then(response => {
+                setWebsiteOptions(response.data);
+            })
+            .catch(error => {
+                console.error('Error fetching website options:', error);
+            });
+    }, []);
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        const formData = {
+            campaignId: CampaignID,
+            placementId: selectedPlacement,
+            websiteId: selectedWebsite,
+        };
+        console.log(formData)
+        try {
+            const response = await axios.post('http://localhost:5001/api/campaignassignment', formData);
+            console.log('Campaign assigned successfully:', response.data);
+            toast.success('Campaign assigned successfully');
+            // Reset form after successful submission
+            setSelectedPlacement("");
+            setSelectedWebsite("");
+            // Set campaignIdResponse state with the received campaignId
+            setCampaignIdResponse(response.data.campaignId);
+            
+           
+            toast({
+            description:response.data.campaignId,
+            position: 'top-center',
+        });
+
+        } catch (error) {
+            console.error('Error assigning campaign:', error);
+            toast.error('Error assigning campaign');
+        }
+    };
+
+    return (
+        <Dialog>
             <DialogTrigger asChild>
                 <Button variant="ghost" className="m-1">
                     Assign Campaign
@@ -29,8 +82,51 @@ export default function AssignCampaign({ CampaignID }) {
                         Make changes to your Campaign. Click save when you're done.
                     </DialogDescription>
                 </DialogHeader>
-                form for Assigning Campaign
+                <form onSubmit={handleSubmit}>
+                    <div className="grid grid-cols-2 gap-4 py-4">
+                        <div>
+                            <Label>Placement:</Label>
+                            <Select onValueChange={(value) => setSelectedPlacement(value)} defaultValue={selectedPlacement}>
+                                <SelectTrigger>
+                                    <SelectValue placeholder="Select a placement" />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    {placementOptions.map(placement => (
+                                        <SelectItem key={placement.id} value={placement._id}>
+                                            {placement.name}
+                                        </SelectItem>
+                                    ))}
+                                </SelectContent>
+                            </Select>
+                        </div>
+                        <div>
+                            <Label>Website:</Label>
+                            <Select onValueChange={(value) => setSelectedWebsite(value)} defaultValue={selectedWebsite}>
+                                <SelectTrigger>
+                                    <SelectValue placeholder="Select a website" />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    {websiteOptions.map(website => (
+                                        <SelectItem key={website.id} value={website._id}>
+                                            {website.url}
+                                        </SelectItem>
+                                    ))}
+                                </SelectContent>
+                            </Select>
+                        </div>
+                    </div>
+                    <DialogFooter className="col-span-2 py-6">
+                        <Button type="submit">Assign</Button>
+                    </DialogFooter>
+                </form>
+                {/* Display campaignIdResponse if it's available */}
+                {campaignIdResponse && (
+                    <div>
+                        <Label>Campaign ID:</Label>
+                        <input type="text" value={campaignIdResponse} readOnly />
+                    </div>
+                )}
             </DialogContent>
         </Dialog>
-  )
+    );
 }
--- adsxclient/src/components/dashboard/publishers/createPlacement.tsx
+++ adsxclient/src/components/dashboard/publishers/createPlacement.tsx
@@ -3,140 +3,148 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogTitle,
-  DialogHeader,
-  DialogTrigger
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogTitle,
+    DialogHeader,
+    DialogTrigger
 } from '@/components/ui/dialog';
 import { PlusCircle } from "lucide-react";
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue
 } from '@/components/ui/select';
 import { toast } from 'sonner';
 import { useForm } from 'react-hook-form';
 
 export default function CreatePlacement() {
-  const [placementData, setPlacementData] = useState({
-    name: "",
-    description: "",
-    websiteId: "",
-    websiteUrl: "",
-    publisherId: "6624c982c54d9f664fb57061",
-    width: 0,
-    height: 0,
-  });
-  const [websites, setWebsites] = useState([]);
-  const form = useForm();
-
-  useEffect(() => {
-    axios.get('http://localhost:5001/api/websites')
-      .then(response => {
-        setWebsites(response.data);
-      })
-      .catch(error => {
-        console.error('Error fetching websites:', error);
-      });
-  }, []);
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setPlacementData({
-      ...placementData,
-      [name]: value,
+    const [placementData, setPlacementData] = useState({
+        name: "",
+        description: "",
+        websiteId: "",
+        websiteUrl: "",
+        publisherId: "6624c982c54d9f664fb57061",
+        width: 0,
+        height: 0,
     });
-  };
+    const [websites, setWebsites] = useState([]);
+    const form = useForm();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+    useEffect(() => {
+        axios.get('http://localhost:5001/api/websites')
+            .then(response => {
+                setWebsites(response.data);
+            })
+            .catch(error => {
+                console.error('Error fetching websites:', error);
+            });
+    }, []);
 
-    // Extract websiteId and websiteUrl from the selected value
-    const [websiteId, websiteUrl] = placementData.websiteId.split(',');
-
-    // Update placementData with websiteId and websiteUrl
-    const updatedPlacementData = {
-      ...placementData,
-      websiteId,
-      websiteUrl
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setPlacementData({
+            ...placementData,
+            [name]: value,
+        });
     };
 
-    try {
-      const response = await axios.post('http://localhost:5001/api/placement', updatedPlacementData);
-      console.log('Placement created successfully:', response.data);
-      toast.success('Placement created successfully');
-      
-    } catch (error) {
-      console.error('Error creating placement:', error);
-      toast.error('Error creating placement');
-    }
-  };
+    const handleSubmit = async (e) => {
+        e.preventDefault();
 
-  return (
-    <Dialog>
-      <DialogTrigger asChild>
-        <Button size="sm" className="ml-auto gap-1">
-          <PlusCircle className="h-3.5 w-3.5" />
-          <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">Create Placement</span>
-        </Button>
-      </DialogTrigger>
-      <DialogContent className="sm:max-w-[650px]">
-        <DialogHeader>
-          <DialogTitle className="my-2">Create New Placement</DialogTitle>
-          <DialogDescription>Fill in the details below to create a new Placement.</DialogDescription>
-        </DialogHeader>
-        <div className="overflow-y-auto">
-          <form onSubmit={handleSubmit}>
-            <div className="grid grid-cols-2 gap-4 py-4">
-              <div>
-                <Label>Name:</Label>
-                <Input type="text" name="name" value={placementData.name} onChange={handleChange} />
-              </div>
-              <div>
-                <Label>Description:</Label>
-                <Input type="text" name="description" value={placementData.description} onChange={handleChange} />
-              </div>
-              <div className="col-span-1">
-                <Label>Website URL:</Label>
-                <Select onValueChange={(value) => setPlacementData(prevData => ({ ...prevData, websiteId: value }))} defaultValue={placementData.websiteId}>
-                  <SelectTrigger>
-                    <SelectValue placeholder="Select a website" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    {websites.map(website => (
-                      <SelectItem key={website._id} value={`${website._id},${website.url}`}>
-                        {website.url}
-                      </SelectItem>
-                    ))}
-                  </SelectContent>
-                </Select>
-              </div>
-              <div className="pt-4 col-span-2">
-                <p>Dimensions</p>
-              </div>
-              <div>
-                <Label>Width:</Label>
-                <Input type="text" name="width" value={placementData.width} onChange={handleChange} />
-              </div>
-              <div>
-                <Label>Height:</Label>
-                <Input type="text" name="height" value={placementData.height} onChange={handleChange} />
-              </div>
-            </div>
-            <DialogFooter className="col-span-2 py-6">
-              <Button type="submit">Save</Button>
-            </DialogFooter>
-          </form>
-        </div>
-      </DialogContent>
-    </Dialog>
-  );
+        // Extract websiteId and websiteUrl from the selected value
+        const [websiteId, websiteUrl] = placementData.websiteId.split(',');
+
+        // Update placementData with websiteId and websiteUrl
+        const updatedPlacementData = {
+            ...placementData,
+            websiteId,
+            websiteUrl
+        };
+
+        try {
+            const response = await axios.post('http://localhost:5001/api/placement', updatedPlacementData);
+            console.log('Placement created successfully:', response.data);
+            toast.success('Placement created successfully');
+            setPlacementData({
+                name: "",
+                description: "",
+                websiteId: "",
+                websiteUrl: "",
+                publisherId: "6624c982c54d9f664fb57061",
+                width: 0,
+                height: 0,
+            })
+        } catch (error) {
+            console.error('Error creating placement:', error);
+            toast.error('Error creating placement');
+        }
+    };
+
+    return (
+        <Dialog>
+            <DialogTrigger asChild>
+                <Button size="sm" className="ml-auto gap-1">
+                    <PlusCircle className="h-3.5 w-3.5" />
+                    <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">Create Placement</span>
+                </Button>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[650px]">
+                <DialogHeader>
+                    <DialogTitle className="my-2">Create New Placement</DialogTitle>
+                    <DialogDescription>Fill in the details below to create a new Placement.</DialogDescription>
+                </DialogHeader>
+                <div className="overflow-y-auto">
+                    <form onSubmit={handleSubmit}>
+                        <div className="grid grid-cols-2 gap-4 py-4">
+                            <div>
+                                <Label>Name:</Label>
+                                <Input type="text" name="name" value={placementData.name} onChange={handleChange} />
+                            </div>
+                            <div>
+                                <Label>Description:</Label>
+                                <Input type="text" name="description" value={placementData.description} onChange={handleChange} />
+                            </div>
+                            <div className="col-span-1">
+                                <Label>Website URL:</Label>
+                                <Select onValueChange={(value) => setPlacementData(prevData => ({ ...prevData, websiteId: value }))} defaultValue={placementData.websiteId}>
+                                    <SelectTrigger>
+                                        <SelectValue placeholder="Select a website" />
+                                    </SelectTrigger>
+                                    <SelectContent>
+                                        {websites.map(website => (
+                                            <SelectItem key={website._id} value={`${website._id},${website.url}`}>
+                                                {website.url}
+                                            </SelectItem>
+                                        ))}
+                                    </SelectContent>
+                                </Select>
+                            </div>
+                            <div className="pt-4 col-span-2">
+                                <p>Dimensions</p>
+                            </div>
+                            <div>
+                                <Label>Width:</Label>
+                                <Input type="text" name="width" value={placementData.width} onChange={handleChange} />
+                            </div>
+                            <div>
+                                <Label>Height:</Label>
+                                <Input type="text" name="height" value={placementData.height} onChange={handleChange} />
+                            </div>
+                        </div>
+                        <DialogFooter className="col-span-2 py-6">
+                            <Button type="submit">Save</Button>
+                        </DialogFooter>
+                    </form>
+                </div>
+            </DialogContent>
+        </Dialog>
+    );
 }
