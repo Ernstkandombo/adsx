6210d369531287ad73333de5804d5196cc1647f5
Ernst Kandombo <51041541+Ernstkandombo@users.noreply.github.com>
2024-04-23 15:05:19 +0200

    n

--- adsxserver/server.js
+++ adsxserver/server.js
@@ -41,7 +41,7 @@ app.get('/', function (req, res) {
 });
 
 // Start server
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 })
