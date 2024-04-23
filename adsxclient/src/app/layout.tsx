112eeb0897db6bd1001ba221f1800f3d28e48204
Ernst Kandombo <51041541+Ernstkandombo@users.noreply.github.com>
2024-04-23 17:18:58 +0200

    x

--- adsxclient/src/app/layout.tsx
+++ adsxclient/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster, toast } from 'sonner'
+import { SessionProvider } from "next-auth/react"
 
 
 
@@ -19,8 +20,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
-        <Toaster richColors  />
+       <SessionProvider session={session}>
+        <body className={inter.className}>{children}</body>
+        <Toaster richColors />
+      </SessionProvider>
     </html>
   );
 }
