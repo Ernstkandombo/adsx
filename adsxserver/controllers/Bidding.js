6716266efabe9020325a7c0918e09a5f20b77e68
Ernst Kandombo <51041541+Ernstkandombo@users.noreply.github.com>
2024-04-29 18:20:59 +0200

    d

--- adsxserver/controllers/Bidding.js
+++ adsxserver/controllers/Bidding.js
@@ -5,8 +5,15 @@ const Placement = require('../models/Placement');
 const Campaign = require('../models/Campaign');
 const Website = require('../models/Website');
 
+// The publisher is bidding for the highest total budget and higest clicks and impression cost, while the advertiser wants the highest views per Website.
+// so the logic should be, the publisher will be this.bidding with the views by providing the website and its Placement, to the bid the the logic take the best of 4 bids.
+// note that the bid algorim, should wait at least for bids for it to Selection, the publisher can bid up to 4 times but not with the same websites which they bid already.
+
+// the algorithm should pupolate the CampaignAssignment, with the details from the assigment after the bid is set.
+
 
 exports.bidding = async (req, res) => {
+
 
 
 
