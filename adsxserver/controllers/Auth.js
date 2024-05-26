d0b668b31dccafdaa567cd102ae497813a2d8eb7
Ernst Kandombo <51041541+Ernstkandombo@users.noreply.github.com>
2024-05-26 15:35:05 +0200

    server

--- adsxserver/controllers/Auth.js
+++ adsxserver/controllers/Auth.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const Advertiser = require('../models/Advertiser');
 const Publisher = require('../models/Publisher');
